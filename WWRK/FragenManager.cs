e06ed5367752b2b6c05040a74bce277fffe21a2c
PhantomPhr3ak <34191989+PhantomPhr3ak@users.noreply.github.com>
2018-12-28 21:57:25 +0100

    DB Test

--- WWRK/FragenManager.cs
+++ WWRK/FragenManager.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.OleDb;
 
 namespace WWRK
 {
     class FragenManager
     {
+        //Datenbank sachen
+        OleDbConnection db_connection = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0; Data Source=c:\bin\Addressen.mdb");
+        private OleDbDataAdapter db_adapter;
+        private OleDbCommand db_command;
+
+
+        //Fragen und Antworten
         private Frage[] FragenTeam1 = new Frage[5];
         private Frage[] FragenTeam2 = new Frage[5];
 
@@ -15,5 +23,7 @@ namespace WWRK
 
         private int aktuellTeam1;
         private int aktuellTeam2;
+
+        
     }
 }
