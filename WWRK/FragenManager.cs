7d5b65b0c4da59239c168a9b1b2c2ebae0913a51
PhantomPhr3ak <34191989+PhantomPhr3ak@users.noreply.github.com>
2018-12-28 21:02:30 +0100

    FragenManager erstellt

---
+++ WWRK/FragenManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WWRK
+{
+    class FragenManager
+    {
+        private Frage[] FragenTeam1 = new Frage[5];
+        private Frage[] FragenTeam2 = new Frage[5];
+
+        Random der_Zufall = new Random();
+
+        private int aktuellTeam1;
+        private int aktuellTeam2;
+    }
+}
