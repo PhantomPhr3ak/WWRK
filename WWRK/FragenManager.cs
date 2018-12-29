c3472b30f626ee7afa238cedb00edb2079295a00
PhantomPhr3ak <34191989+PhantomPhr3ak@users.noreply.github.com>
2018-12-29 18:44:34 +0100

    Methode AlleFragenLaden funktionsfähig gemacht

--- WWRK/FragenManager.cs
+++ WWRK/FragenManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,15 +11,18 @@ namespace WWRK
 {
     class FragenManager
     {
+        //https://docs.microsoft.com/de-de/dotnet/framework/data/adonet/populating-a-dataset-from-a-dataadapter
         //Datenbank sachen
         OleDbConnection db_connection = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0; Data Source=c:\bin\Addressen.mdb");
         private OleDbDataAdapter db_adapter;
         private OleDbCommand db_command;
+        private DataSet alleFragenDataSet;
 
 
         //Fragen und Antworten
         private Frage[] fragenTeam1 = new Frage[5];
         private Frage[] fragenTeam2 = new Frage[5];
+        private Frage[] alleFragen;
 
         Random der_Zufall = new Random();
 
@@ -27,16 +31,41 @@ namespace WWRK
 
         private Form1 form;
 
+        private int i, j, k;
+
         FragenManager(Form1 _form)
         {
             form = _form;
-
-            db_command.Connection = db_connection;
+            
+            AlleFragenLaden();
+            FragenAuswählen();
         }
 
         public void AlleFragenLaden()
         {
-            db_command.CommandText = "Select * FROM Fragen";
+            //Daten aus DB laden
+            db_adapter = new OleDbDataAdapter("SELECT * FROM Fragen", db_connection);
+            db_adapter.Fill(alleFragenDataSet, "Fragen");
+
+            //Fragen vom DataSet ins Array
+            if (alleFragenDataSet != null)
+            {
+                i = 0;
+
+                foreach (DataRow row in alleFragenDataSet.Tables["Fragen"].Rows)
+                {
+                    //einzelne Frage befüllen
+                    Frage _frage = new Frage(
+                        row["Frage"].ToString(),
+                        row["Antwort1"].ToString(),
+                        row["Antwort2"].ToString(),
+                        row["Antwort3"].ToString(),
+                        row["Antwort4"].ToString());
+
+                    //Frage ins Array einfügen
+                    alleFragen[i] = _frage;
+                }
+            }
         }
 
         public void FragenAuswählen()
@@ -51,7 +80,7 @@ namespace WWRK
 
         public void AntwortBestätigen(string ausgewählteAntwort)
         {
-            
+
         }
     }
 }
